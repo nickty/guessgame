6c4b0a4943c2484c947128c16813f78a12604012
Mizanur Rahman <mizanur.rahman@navana.com>
2022-08-29 06:49:54 +0600

    button ripple effect

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,16 +1,35 @@
 /** @format */
 
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Pressable } from 'react-native';
 import React, { Children } from 'react';
 
 const PrimaryButton = ({ children }) => {
   return (
-    <View>
-      <Text>{children}</Text>
+    <View style={styles.outerContainter}>
+      <Pressable android_ripple={{ color: '#000000' }} style={styles.container}>
+        <Text style={styles.buttonText}>{children}</Text>
+      </Pressable>
     </View>
   );
 };
 
 export default PrimaryButton;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  outerContainter: {
+    borderRadius: 28,
+    margin: 4,
+    overflow: 'hidden',
+  },
+  container: {
+    backgroundColor: '#ffffff',
+    // borderRadius: 25,
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    elevation: 2,
+    // margin: 4,
+  },
+  buttonText: {
+    textAlign: 'center',
+  },
+});
--- screens/StarGameScreen.js
+++ screens/StarGameScreen.js
@@ -6,8 +6,14 @@ import PrimaryButton from '../components/PrimaryButton';
 
 const StarGameScreen = () => {
   return (
-    <View>
-      <TextInput />
+    <View style={styles.inputContainer}>
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType='number-pad'
+        autoCapitalize='none'
+        autoCorrect={false}
+      />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
@@ -16,4 +22,25 @@ const StarGameScreen = () => {
 
 export default StarGameScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  inputContainer: {
+    // flex: 1,
+    marginTop: 100,
+    marginHorizontal: 24,
+    borderRadius: 8,
+    padding: 16,
+    backgroundColor: '#72063c',
+    elevation: 4,
+  },
+  numberInput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: '#ddb52f',
+    borderBottomWidth: 2,
+    color: '#ddb52f',
+    marginVertical: 8,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
