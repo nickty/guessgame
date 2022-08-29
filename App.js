8377d2799293041241b26f7ea49787a9a3970064
Mizanur Rahman <mizanur.rahman@navana.com>
2022-08-29 21:25:42 +0600

    last updte

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
 /** @format */
 
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text } from 'react-native';
 import StarGameScreen from './screens/StarGameScreen';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function App() {
-  return <StarGameScreen />;
+  return (
+    <LinearGradient style={styles.screen}>
+      <StarGameScreen />
+    </LinearGradient>
+  );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  screen: {
+    backgroundColor: '#ddb52f',
+    flex: 1,
+  },
+});
