4c013a87a47c2ba8a31b0242b8af55bb5551ecf9
nickty <nickty.86@gmail.com>
2022-08-30 22:58:29 +0600

    conditionality loading screen

--- App.js
+++ App.js
@@ -3,13 +3,25 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, ImageBackground } from 'react-native';
 import StarGameScreen from './screens/StarGameScreen';
-import { LinearGradient} from 'expo-linear-gradient'
+import { LinearGradient} from 'expo-linear-gradient' 
+import React, { useState } from 'react'
+import GameScreen from './screens/GameScreen';
 
 export default function App() {
+  const [first, setfirst] = useState() 
+
+  function pickNumberHanlder(pickedNumber){
+    setfirst(pickedNumber)
+  }
+
+  let screen = <StarGameScreen onPickedNumber = {pickNumberHanlder} />
+  if(first){
+    screen = <GameScreen />
+  }
   return (
     <LinearGradient colors={['#000000', '#ffffff ']} style={styles.screen}>
      <ImageBackground style={styles.screen} imageStyle={styles.backImage} source={require('./assets/splash.png')} resizeMode="cover">
-     <StarGameScreen />
+     {screen}
      </ImageBackground>
       
     </LinearGradient>
